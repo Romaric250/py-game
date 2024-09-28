6333b976170d21c9a0f690c73f6e9b846707b09f
Romaric250 <lonfonyuyromeo@gmail.com>
2024-09-28 13:46:08 +0100

    stashing new changes here

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 
 
 pygame.init()
-screen = pygame.display.set_mode((1250,750))
+screen = pygame.display.set_mode((750,500))
 clock = pygame.time.Clock()
 running = True 
 dt = 0
@@ -13,6 +13,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill('purple')
+
+    pygame.draw.circle(screen, 'pink',(100,500),30)
+
+
+
+
 
     pygame.display.flip()
     clock.tick(60)
