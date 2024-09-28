db2f3b21aa3dae25ba8d2122d1811c50b3c06ef7
Romaric250 <lonfonyuyromeo@gmail.com>
2024-09-28 13:36:49 +0100

    working on stashing some important feutues

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("testing some content here")
+import pygame
 
+
+pygame.init()
+screen = pygame.display.set_mode((1250,750))
+clock = pygame.time.Clock()
+running = True 
+dt = 0
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill('purple')
+
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit
